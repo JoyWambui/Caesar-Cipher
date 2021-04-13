961446bf9964f38cdc2d40d3ee864b0c137d5231
JoyWambui <JoyWambui@github.com>
2021-04-13 13:21:56 +0300

    import necessary java imports to read from console

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,5 +1,11 @@
-
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class App {
+    public static void main(String[]args)throws Exception{
+
+
+    }
 
 }
