3545711cc0d62dc7eead1d4e7319e003bfea633d
JoyWambui <JoyWambui@github.com>
2021-04-13 13:50:57 +0300

    add navigation options to encrypt, decrypt, or exit from app

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -4,7 +4,24 @@ import java.io.InputStreamReader;
 
 public class App {
     public static void main(String[]args)throws Exception{
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Welcome to the Caesar-Cipher Program!");
+        System.out.println("Enter one option: Encrypt, Decrypt, Exit.");
+        System.out.println(":");
 
+        String chosenOption = reader.readLine();
+        if (chosenOption.equals("Encrypt")){
+            System.out.println("Enter Sentence to be encrypted: ");
+
+        }else if(chosenOption.equals("Decrypt")){
+            System.out.println("Enter Sentence to be decrypted: ");
+
+        }else if(chosenOption.equals("Exit")){
+            System.out.println("Goodbye!");
+
+        }else{
+            System.out.println("Kindly choose one of the options!");
+        }
 
     }
 
